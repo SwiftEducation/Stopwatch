f1a5101fad1c05fb154703ad64eaa200abcea672
Yong Bakos <ybakos@humanoriented.com>
2015-05-14 17:19:37 -0500

    Lesson 2. Add Stopwatch model.

---
+++ Stopwatch/Stopwatch.swift
@@ -0,0 +1,30 @@
+/*
+
+This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike
+4.0 International License, by Yong Bakos.
+
+*/
+
+import Foundation
+
+class Stopwatch {
+    
+    private var startTime: NSDate?
+
+    var elapsedTime: NSTimeInterval {
+        if let startTime = self.startTime {
+            return -startTime.timeIntervalSinceNow
+        } else {
+            return 0
+        }
+    }
+    
+    func start() {
+        startTime = NSDate()
+    }
+    
+    func stop() {
+        startTime = nil
+    }
+    
+}
